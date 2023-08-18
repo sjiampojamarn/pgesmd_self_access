502ea51f309ee758730031948639ac636a200cab
sjiampojamarn <18257803+sjiampojamarn@users.noreply.github.com>
2023-08-17 20:55:44 -0700

    Create request_historical_data.py

---
+++ request_historical_data.py
@@ -0,0 +1,13 @@
+from pgesmd_self_access.api import SelfAccessApi
+from pgesmd_self_access.server import SelfAccessServer
+from pgesmd_self_access.helpers import save_espi_xml
+
+def main():
+  pge_api = SelfAccessApi.auth('/root/auth/auth.json')
+  pge_api.request_historical_data(60)
+ 
+
+# Using the special variable 
+# __name__
+if __name__=="__main__":
+  main()
